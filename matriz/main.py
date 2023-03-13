7c7ee367e10c8aeffc18e2fc8f95cfea1dcf4485
Facundo <facugastonbarral@gmail.com>
2023-03-13 18:57:41 -0300

    Created exercise main file

---
+++ matriz/main.py
@@ -0,0 +1,5 @@
+"""
+    Crear una matriz de 5x5 randomizada con números enteros, encontrar secuencia de 4
+    números consecutivos horizontal o vertical y si se encuentra mostrar la posición inicial y
+    final.
+"""
