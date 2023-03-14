b5f13790173305cebe45788a1292a1c572168e29
Facundo <facugastonbarral@gmail.com>
2023-03-14 00:21:19 -0300

    Created exercise main file

---
+++ clases/main.py
@@ -0,0 +1,11 @@
+"""
+    Escribir una clase en python llamada círculo que contenga un radio, con un método que
+    devuelva el área y otro que devuelva el perímetro del círculo.
+    Si se instancia la clase con radio <= 0 mostrar una excepción indicando un error amigable al
+    usuario e impidiendo la instanciación.
+    Si printeamos el objeto creado debe mostrarse una representación amigable.
+    El objeto debe tener su atributo radio modificable, si se le intenta setear un valor <= 0
+    mostrar un error y no permitir modificación.
+    Permitir la multiplicación del circulo: Circulo * n debe devolver un nuevo objeto con el radio
+    multiplicado por n. No permitir la multiplicación por números <= 0
+"""
