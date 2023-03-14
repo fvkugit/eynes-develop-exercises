f6da6f7e5ee45ce68d112dbbaa40a133f41b5737
Facundo <facugastonbarral@gmail.com>
2023-03-13 22:54:41 -0300

    Created table generator function

--- matriz/main.py
+++ matriz/main.py
@@ -3,3 +3,10 @@
     números consecutivos horizontal o vertical y si se encuentra mostrar la posición inicial y
     final.
 """
+
+import random 
+
+def numbersTable(length):
+    # Create 5x5 table of random numbers between 0 and 9 
+    table = [[random.randint(0, 9) for j in range(length)] for i in range(length)]
+    return table
