9ffb32a3adac66d5d52bc68319921955978138bf
Facundo <facugastonbarral@gmail.com>
2023-03-13 23:42:26 -0300

    Added docstring & doctest to numbersTable function

--- matriz/main.py
+++ matriz/main.py
@@ -3,10 +3,29 @@
     números consecutivos horizontal o vertical y si se encuentra mostrar la posición inicial y
     final.
 """
-
 import random 
+import doctest
+
 
 def numbersTable(length):
+    """
+        Generate a table of random numbers (length x length)
+
+        Args:
+            length (int): Length of rows and columns on the table
+
+        Returns:
+            table (list): List of rows 
+        
+        Example:
+            # Test generating a 5x5 table with random numbers
+            >>> random.seed(1) # Ensure consistent results
+            >>> numbersTable(5)
+            [[2, 9, 1, 4, 1], [7, 7, 7, 6, 3], [1, 7, 0, 6, 6], [9, 0, 7, 4, 3], [9, 1, 5, 0, 0]]
+            >>> random.seed(3) # Ensure consistent results
+            >>> numbersTable(7)
+            [[3, 9, 8, 2, 5, 9, 7], [9, 1, 9, 0, 7, 4, 8], [3, 3, 7, 8, 8, 7, 6], [2, 3, 2, 8, 6, 0, 1], [2, 9, 0, 4, 0, 4, 7], [9, 6, 6, 6, 9, 7, 2], [5, 1, 0, 2, 7, 3, 4]]
+    """
     # Create 5x5 table of random numbers between 0 and 9 
     table = [[random.randint(0, 9) for j in range(length)] for i in range(length)]
     return table
@@ -29,3 +48,6 @@ def findSequence(table):
 def printTable(table):
     for row in table:
         print(row)
+
+if __name__ == "__main__":
+    doctest.testmod()
